0d694abbe1ec26c1916012b44beac6489548dbce
Alistair Taft <TaftA@ract.com.au>
2016-07-20 13:23:14 +1000

    Added missing webpack loaders

--- webpack.base.config.js
+++ webpack.base.config.js
@@ -28,7 +28,11 @@ const webpackConfig = {
       { test: /\.woff2(\?v=\d+\.\d+\.\d+)?$/,   loader: "url?limit=10000&mimetype=application/font-woff2" },
       { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=application/octet-stream" },
       { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/,    loader: "file" },
-      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" }
+      { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,    loader: "url?limit=10000&mimetype=image/svg+xml" },
+      {
+        test: /\.scss$/,
+        loaders: ["style", "css", "sass"]
+      }
     ],
   }
 }
