0950081065d15f6445a605de739d4ef0a07100d8
CasperGrey <mrcaspergrey@gmail.com>
2016-08-03 16:36:53 +1000

    Fixed FB Login Component

--- src/modules/LoginPage/LoginPage.js
+++ src/modules/LoginPage/LoginPage.js
@@ -7,34 +7,35 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-import React, { Component, PropTypes } from 'react';
-import withStyles from 'isomorphic-style-loader/lib/withStyles';
-import s from './LoginPage.css';
+import React from 'react';
+import FacebookLogin from './FacebookLogin';
 
-const FBSDK = require('react-native-fbsdk');
-const {
-    LoginButton,
-} = FBSDK;
+class Login extends React.Component{
 
-var Login = React.createClass({
-  render: function() {
-    return (
-        <View>
-          <LoginButton
-              publishPermissions={["publish_actions"]}
-              onLoginFinished={
-                (error, result) => {
-                  if (error) {
-                    alert("Login failed with error: " + result.error);
-                  } else if (result.isCancelled) {
-                    alert("Login was cancelled");
-                  } else {
-                    alert("Login was successful with permissions: " + result.grantedPermissions)
-                  }
-                }
-              }
-              onLogoutFinished={() => alert("User logged out")}/>
-        </View>
-    );
-  }
-});
+    constructor (props, context) {
+        super(props, context);
+    }
+
+    responseFacebook (response) {
+        console.log(response);
+        //anything else you want to do(save to localStorage)...
+    }
+
+    render () {
+        return (
+            <div>
+                <FacebookLogin socialId="1648042272179054"
+                               language="en_US"
+                               scope="public_profile,email"
+                               responseHandler={this.responseFacebook}
+                               xfbml={true}
+                               version="v2.5"
+                               class="facebook-login"
+                               buttonText="Login With Facebook"/>
+            </div>
+        );
+    }
+
+}
+
+export default Login;
