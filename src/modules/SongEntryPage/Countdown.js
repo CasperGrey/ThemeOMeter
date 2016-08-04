8ff345304f32d7c758cf37ba917738d11529d28d
CasperGrey <mrcaspergrey@gmail.com>
2016-08-04 16:38:14 +1000

    Countdown module updates

--- src/modules/SongEntryPage/Countdown.js
+++ src/modules/SongEntryPage/Countdown.js
@@ -12,7 +12,10 @@ export default class Countdown extends React.Component {
 
     this.state = {
       completed: 0,
-      themeDate: moment().endOf('week').subtract('days',1)
+      themeDate: moment().endOf('week').subtract('days',1),
+      daysLeft: 0,
+      minutesLeft: 0,
+      hoursLeft: 0
     };
   }
 
@@ -30,16 +33,22 @@ export default class Countdown extends React.Component {
     const { themeDate } = this.state
     var today = moment()
     const diff = themeDate.diff(today,'milliseconds');
+    const daysDiff = themeDate.diff(today,'days');
+    const hoursDiff = themeDate.diff(today,'hours');
+    const minutesDiff = themeDate.diff(today,'minutes');
     const millisecondsInAWeek = 1000 * 60 * 60 * 24 * 7
     this.setState({ 
-      completed: (millisecondsInAWeek - diff) / millisecondsInAWeek * 100,
+        completed: (millisecondsInAWeek - diff) / millisecondsInAWeek * 100,
+        daysLeft: daysDiff,
+        minutesLeft:minutesDiff,
+        hoursLeft: hoursDiff,
     })
   }
 
   render() {
     return <div className="Countdown">
       <LinearProgress mode="determinate" value={this.state.completed} />
-      <span>{this.state.completed}</span>
+      <span>{this.state.daysLeft} Day {this.state.hoursLeft} Hours {this.state.minutesLeft} Minutes</span>
     </div>
   }
 }
