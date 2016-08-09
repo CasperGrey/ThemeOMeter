7b346b686eaccd66662d1e2daa3ef089a9c41ae1
CasperGrey <mrcaspergrey@gmail.com>
2016-08-09 19:17:46 +1000

    - Added new helper functions to entry  API --getSongsInTheme()

--- src/db/entry.js
+++ src/db/entry.js
@@ -9,11 +9,11 @@ router.use(bodyParser.json())
 import connection from './db.js'
 
 
-export function getScoreBySongId(id){
+export function getSongsInTheme(id){
     var value = req.body.songName
 
     return new Promise(function(resolve, reject){
-        connection.query('SELECT top 1 from factscores where song_id = ?', [id], function(err, rows, fields) {
+        connection.query('SELECT * from factentry where theme_id = ?', [id], function(err, rows, fields) {
             if (err) return reject(err);
 
             var result = rows[0]
