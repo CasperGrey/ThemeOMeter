f3e3ebaae77910a09f2cc34acae9b495b375e6ba
CasperGrey <mrcaspergrey@gmail.com>
2016-08-09 19:03:11 +1000

    - Added new helper functions to artists API --getArtistID() --getSongsByArtist() --createArtist()

--- src/db/artists.js
+++ src/db/artists.js
@@ -13,7 +13,7 @@ export function getArtist(name){
     var value = req.body.songName
 
     return new Promise(function(resolve, reject){
-        connection.query('SELECT top 1 from dimartists where name = ?', [name], function(err, rows, fields) {
+        connection.query('SELECT top 1 from dimartists where artist_name = ?', [name], function(err, rows, fields) {
             if (err) return reject(err);
 
             var result = rows[0]
@@ -23,3 +23,54 @@ export function getArtist(name){
         });
     })
 }
+
+export function getArtistID(name){
+    var value = req.body.songName
+
+    return new Promise(function(resolve, reject){
+        connection.query('SELECT artist_id FROM dimartists where artist_name like "?"', [name], function(err, rows, fields) {
+            if (err) return reject(err);
+
+            var result = rows[0]
+
+            resolve(result)
+
+        });
+    })
+}
+
+
+export function getSongsByArtist(id){
+    var value = req.body.artistName
+
+    return new Promise(function(resolve, reject){
+        connection.query('SELECT * FROM dimsongs where artist_id = ?', [id] , function(err, rows, fields) {
+            if (err) return reject(err);
+
+            var result = rows[0]
+
+            resolve(result)
+
+        });
+    })
+}
+
+
+export function createArtist(name){
+    var value = req.body.artistName
+
+    return new Promise(function(resolve, reject){
+        connection.query('INSERT INTO dimartists (artist_name) VALUES(?)', [name] , function(err, rows, fields) {
+            if (err) return reject(err);
+
+            var result = rows[0]
+
+            resolve(result)
+
+        });
+    })
+}
+
+
+
+
