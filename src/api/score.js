37dcdc8ffa2e92abf785d6b54525a4170d4e9402
Alastair Taft <ali.c.taft@gmail.com>
2016-08-29 21:56:31 +1000

    Fixed syntax errors that prevented the app building.

--- src/api/score.js
+++ src/api/score.js
@@ -7,27 +7,22 @@ var router = express.Router();
 router.use(bodyParser.json())
 
 
-import {enterScore} from './../db/.js'
+import {enterScore} from './../db/score.js'
 
 
 // route with parameters (http://localhost:8080/hello/:name)
 router.post('/', async function(req, res, next) {
 
-        var theme_id = req.body.theme_id
-        var song_id = req.body.song_id
-        var score = req.body.score
-        var song_comment = req.body.song_comment
+    var theme_id = req.body.theme_id
+    var song_id = req.body.song_id
+    var score = req.body.score
+    var song_comment = req.body.song_comment
 
     try {
-            var score = await enterScore(theme_id,song_id,score,song_comment)
-
-        }
-    }catch(err){
-
+        var score = await enterScore(theme_id,song_id,score,song_comment)
+    } catch(err){
         console.error(err)
         next(err)
-
-
     }
 
 
