b41945d69558dad9c8486c0512750aa8c1a2fc24
CasperGrey <mrcaspergrey@gmail.com>
2016-08-30 12:19:38 +1000

    Updated Container to match Ali's commit

--- src/modules/ThemeScoringPage/Container.jsx
+++ src/modules/ThemeScoringPage/Container.jsx
@@ -26,19 +26,19 @@ class Container extends Component {
         }
         else {
 
-            fetch('/api/score', {
-                method: 'post',
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                body: JSON.stringify({
-                    theme_id: songs.theme_id,
-                    artist_id: songs.song_id,
-                    url: songs.score,
-                    videoId: songs.song_comment,
+                fetch('/api/score', {
+                    method: 'post',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        theme_id: songs.theme_id,
+                        artist_id: songs.song_id,
+                        url: songs.score,
+                        videoId: songs.song_comment,
+                    })
                 })
-            })
-            
+            }
         }
     };
 
@@ -48,6 +48,7 @@ class Container extends Component {
         {...this.props}
         songs={this.state.songs}
         currentTheme={this.state.currentTheme}
+        onSave={this.onSave}
     />
 }
 
