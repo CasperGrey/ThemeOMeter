7cf3964bbc5272a55da38840572a7a405c69da82
CasperGrey <mrcaspergrey@gmail.com>
2016-09-20 16:20:04 +1000

    updated react look styles for fb button

--- src/modules/LoginPage/FacebookLogin.js
+++ src/modules/LoginPage/FacebookLogin.js
@@ -53,10 +53,18 @@ export default class FacebookLogin extends React.Component {
     render() {
         return (
             <div>
-                <RaisedButton className={this.props.class} onClick={this.clickHandler.bind(this)} style={color:'#4267B2'}>
+                <RaisedButton className={`${this.props.class} ${styles[‘facebook-login’]}`} onClick={this.clickHandler.bind(this)}>
                     {this.props.buttonText}
                 </RaisedButton>
             </div>
         );
     }
 }
+const styles = StyleSheet.create({
+
+    facebook-login: {
+        color: 'blue',
+
+    },
+
+})
