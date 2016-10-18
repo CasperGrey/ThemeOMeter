b915ccde1fb851e4e27f66e8cba6a81f59c76ac8
CasperGrey <mrcaspergrey@gmail.com>
2016-10-18 20:23:15 +1100

    state change again on handle change

--- src/modules/AdminPage/AdminPage.js
+++ src/modules/AdminPage/AdminPage.js
@@ -36,7 +36,7 @@ class AdminPage extends Component {
 
 
 
-  handleChange = (event, index, value) => this.setState({theme_id: {value}});
+  handleChange = (event, index, value) => this.setState({theme_id: value});
 
   render() {
     var { onSave} = this.props
