321ed1f5f12c6497a36624f22cec32e202af21d9
CasperGrey <mrcaspergrey@gmail.com>
2016-10-19 16:06:18 +1100

    Added try catch on score

--- src/api/score.js
+++ src/api/score.js
@@ -13,15 +13,16 @@ import {enterScore,getScoreBySongId} from './../db/score.js'
 // route with parameters (http://localhost:8080/hello/:name)
 router.post('/', async function(req, res, next) {
 
-    var theme_id = req.body.theme_id
-    var user_id = req.body.user_id
-    var song_id = req.body.song_id
-    var score = req.body.score
-    var song_comment = req.body.song_comment
-
-    console.log(require('util').inspect(req.body))
-
     try {
+
+      var theme_id = req.body.theme_id
+      var user_id = req.body.user_id
+      var song_id = req.body.song_id
+      var score = req.body.score
+      var song_comment = req.body.song_comment
+
+      console.log(require('util').inspect(req.body))
+
       var entry = await getScoreBySongId(song_id)
 
       if(!entry){
@@ -29,7 +30,7 @@ router.post('/', async function(req, res, next) {
       }
     } catch(err){
         console.error(err)
-        next(err)
+        reject(err)
     }
 
 
