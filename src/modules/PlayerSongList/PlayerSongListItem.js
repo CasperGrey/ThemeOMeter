08b8a155250069c39e985213f66c27f79b70a210
CasperGrey <mrcaspergrey@gmail.com>
2016-10-27 07:44:33 +1100

    Removed redundant comment text

--- src/modules/PlayerSongList/PlayerSongListItem.js
+++ src/modules/PlayerSongList/PlayerSongListItem.js
@@ -49,10 +49,6 @@ class PlayerSongListItem extends React.Component {
                         <img className="media-object" src={imageUrl}/>
                     </td>
                     <td>
-                      // TODO: Placeholder for facts about music chosen.
-                        // <div className = {styles.extraSongInfo}>
-                        //     comment about music
-                        // </div>
                         <br />
                         <TextField
                             floatingLabelText={"Artist"}
