3668069034534f33fc698958c4ecf5a070cf57da
CasperGrey <mrcaspergrey@gmail.com>
2016-10-29 09:19:52 +1100

    Created line graph through Recharts NPM Removed Db check fro existing song for Alpha test

--- src/modules/SummaryPage/SummaryPage.js
+++ src/modules/SummaryPage/SummaryPage.js
@@ -13,8 +13,19 @@ import s from './SummaryPage.css';
 import {Card, CardActions, CardTitle,CardMedia} from 'material-ui/Card';
 import Paper from 'material-ui/Paper';
 import { StyleSheet } from 'react-look'
+import {LineChart,Line} from 'recharts'
+
 const title = 'Summary Page';
 
+const data = [
+      {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
+      {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
+      {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
+      {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
+      {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
+      {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
+      {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+];
 
 class SummaryPage extends Component {
 
@@ -42,6 +53,9 @@ class SummaryPage extends Component {
                 <img src="/james-jean-talib.jpg" />
               </CardMedia>
               <CardTitle title="Summary"/>
+                <LineChart width={400} height={400} data={data}>
+                  <Line type="monotone" dataKey="uv" stroke="#8884d8" />
+                </LineChart>
               <CardActions>
               </CardActions>
             </Card>
