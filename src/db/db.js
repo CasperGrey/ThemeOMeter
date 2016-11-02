dcec81b7299f488aa9a8fa9633814d51ce3d8b6d
CasperGrey <mrcaspergrey@gmail.com>
2016-11-03 08:49:18 +1100

    fixed DB

--- src/db/db.js
+++ src/db/db.js
@@ -1,19 +1,19 @@
 import mysql from 'mysql'
 
-// var connection = mysql.createConnection({
-//     host     : 'sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//     user     : 'x8ivvovilk7yv04i',
-//     password : 'oir3i2jx3tr2pbv9',
-//     database : 'aqfcbi1hdv24qagc'
-// });
-
-
 var connection = mysql.createConnection({
-    host     : 'localhost',
-    user     : 'root',
-    password : 'Password1',
-    database : 'thegreys_weekly_music'
+    host     : 'sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user     : 'x8ivvovilk7yv04i',
+    password : 'oir3i2jx3tr2pbv9',
+    database : 'aqfcbi1hdv24qagc'
 });
+
+
+// var connection = mysql.createConnection({
+//     host     : 'localhost',
+//     user     : 'root',
+//     password : 'Password1',
+//     database : 'thegreys_weekly_music'
+// });
 
 connection.connect();
 
