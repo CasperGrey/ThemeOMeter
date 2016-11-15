a16856fd40cb8a98244f9b0fe35a0b1801319938
CasperGrey <mrcaspergrey@gmail.com>
2016-11-16 08:37:09 +1100

    Signed-off-by: CasperGrey <mrcaspergrey@gmail.com>

--- src/db/db.js
+++ src/db/db.js
@@ -17,10 +17,10 @@ import mysql from 'mysql'
 
 
 var connection = mysql.createConnection({
--    host     : 'sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
- -    user     : 'x8ivvovilk7yv04i',
- -    password : 'oir3i2jx3tr2pbv9',
- -    database : 'aqfcbi1hdv24qagc'
+     host     : 'sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+     user     : 'x8ivvovilk7yv04i',
+     password : 'oir3i2jx3tr2pbv9',
+     database : 'aqfcbi1hdv24qagc'
 });
 connection.connect();
 
