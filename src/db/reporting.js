250d844e7427e5223c7c51b4aa970e7c010df00d
CasperGrey <mrcaspergrey@gmail.com>
2016-11-16 19:59:45 +1100

    Signed-off-by: CasperGrey <mrcaspergrey@gmail.com>

--- src/db/reporting.js
+++ src/db/reporting.js
@@ -11,7 +11,7 @@ import connection from './db.js'
 
 export function getCurrentAgentAverageScores(agent_id,theme_id){
     return new Promise(function(resolve, reject){
-        connection.query('SELECT da.agent_name, avg(fs.song_raw_score) FROM factscores fs JOIN dimagents da on fs.agent_id = fs.agent_id WHERE fs.song_picker_id = ? AND theme_id = ? and fs.valid_vote = 1;', [agent_id,theme_id], function(err, rows, fields) {
+        connection.query('SELECT da.agent_name, avg(fs.song_raw_score) FROM factscores fs JOIN dimagents da on da.agent_id = fs.agent_id WHERE fs.song_picker_id = ? AND theme_id = ? and fs.valid_vote = 1 group by da.agent_name;', [agent_id,theme_id], function(err, rows, fields) {
             if (err) return reject(err);
 
             var result = rows
