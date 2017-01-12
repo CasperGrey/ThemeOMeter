698f4b33a03f4ae7e5711f9d1c85cf42ee328817
CasperGrey <mrcaspergrey@gmail.com>
2017-01-13 08:31:38 +1100

    Signed-off-by: CasperGrey <mrcaspergrey@gmail.com>

--- src/db/db.js
+++ src/db/db.js
@@ -8,20 +8,20 @@ import mysql from 'mysql'
 // });
 
 //
-// var connection = mysql.createConnection({
-//     host     : 'localhost',
-//     user     : 'root',
-//     password : 'Password1',
-//     database : 'thegreys_weekly_music'
-// });
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'Password1',
+    database : 'thegreys_weekly_music'
+});
 
 //
-var connection = mysql.createConnection({
-     host     : 'sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-     user     : 'x8ivvovilk7yv04i',
-     password : 'oir3i2jx3tr2pbv9',
-     database : 'aqfcbi1hdv24qagc'
-});
+// var connection = mysql.createConnection({
+//      host     : 'sp6xl8zoyvbumaa2.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+//      user     : 'x8ivvovilk7yv04i',
+//      password : 'oir3i2jx3tr2pbv9',
+//      database : 'aqfcbi1hdv24qagc'
+// });
 connection.connect();
 
 export default connection
